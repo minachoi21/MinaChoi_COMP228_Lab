c80e8c7f74b0e2d107f5b72080189e8f642686e5
Mina <bellella13@gmail.com>
2024-09-13 20:03:21 -0400

    Excercise1 add

--- src/main/java/excercise1/Main.java
+++ src/main/java/excercise1/Main.java
@@ -2,6 +2,16 @@ package excercise1;
 
 public class Main {
     public static void main(String[] args) {
+        Singers mina = new Singers();
 
+        mina.printSinger();
+
+        mina.setId(301083944);
+        mina.setName("Mina Choi");
+        mina.setAddress("2333 danforth ave");
+        mina.setBirthday("1993-07-17");
+        mina.setAlbumsCount(2);
+
+        mina.printSinger();
     }
 }
--- src/main/java/excercise1/Singers.java
+++ src/main/java/excercise1/Singers.java
@@ -1,6 +1,107 @@
 package excercise1;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Singers {
+    private int id;
+    private String name;
+    private String address;
+    private Date birthday;
+    private int albumsCount;
 
+    public Singers() {
+        this.id = 0;
+        this.name = "";
+        this.address = "";
+        this.birthday = new Date();
+        this.albumsCount = 0;
+    }
+
+    public Singers(int id) {
+        this();
+        this.id = id;
+    }
+
+    public Singers(int id, String name) {
+        this(id);
+        this.name = name;
+    }
+
+    public Singers(int id, String name, String address) {
+        this(id, name);
+        this.address = address;
+    }
+
+    public Singers(int id, String name, String address, Date birthday) {
+        this(id, name, address);
+        this.birthday = birthday;
+    }
+
+    public Singers(int id, String name, String address, Date birthday, int albumsCount) {
+        this(id, name, address, birthday);
+        this.albumsCount = albumsCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            this.birthday = dateFormat.parse(birthday);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int getAlbumsCount() {
+        return albumsCount;
+    }
+
+    public void setAlbumsCount(int albumsCount) {
+        this.albumsCount = albumsCount;
+    }
+
+    public void setSinger(int id, String name, String address, Date birthday, int albumsCount) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.birthday = birthday;
+        this.albumsCount = albumsCount;
+    }
+
+    public void printSinger() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Address: " + address + ", Birthday: " + birthday + ", Number of Albums Published: " + albumsCount);
+    }
 }
-
